613f73d8d14331e6dcbf6feb73e4b49d4323b8a3
NuayHL <liuhaoyuan@akane.waseda.jp>
2022-08-02 00:32:45 +0900

    initial framework

---
+++ config.py
@@ -0,0 +1,38 @@
+from odcore.config import CN
+from odcore.config import get_default_cfg as _get_default_cfg
+
+c = _get_default_cfg()
+
+c.model = CN()
+c.model.backbone = 'darknet53'
+c.model.neck = 'yolov3'
+c.model.detector = 'standard_yolo'
+c.model.use_anchor = False
+c.model.fpnlevels = [3, 4, 5]
+c.model.anchor_ratios = [2, 4]
+c.model.anchor_scales = [0.75, 1]
+c.model.assignment = 'default'
+
+c.loss = CN()
+c.loss.reg_type = ['giou','l1']
+c.loss.cls_type = 'bce'
+c.loss.use_focal = False
+c.loss.focal_alpha = 0.25
+c.loss.focal_gamma = 2.0
+
+c.inference = CN()
+
+def get_default_cfg():
+    return c.clone()
+
+def get_default_yaml_templete():
+    cfg_string = get_default_cfg().dump()
+    with open("default_config.yaml", "w") as f:
+        f.write(cfg_string)
+
+if __name__ == "__main__":
+    cfg = get_default_cfg()
+    print(type(cfg))
+    cfg.merge_from_file('default_config.yaml')
+    print(cfg.exp_name)
+    #get_default_yaml_templete()
