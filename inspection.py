44b6f5e7e22e91b6487ee98ce43b1a8ad1c0ed28
NauyHL <liuhaoyuan@akane.waseda.jp>
2022-08-04 23:21:47 +0900

    debug training

--- inspection.py
+++ inspection.py
@@ -4,6 +4,7 @@ path = os.getcwd()
 sys.path.append(os.path.join(path,'odcore'))
 from odcore.data.dataloader import build_dataloader
 from odcore.data.dataset import CocoDataset
+from odcore.data.data_augment import *
 from config import get_default_cfg
 from odcore.utils.visualization import show_bbox, dataset_inspection
 import numpy as np
@@ -15,17 +16,21 @@ cfg.merge_from_file('test_config.yaml')
 dataset = CocoDataset('CrowdHuman/annotation_train_coco_style_100.json','CrowdHuman/Images_train',config_data=cfg.data, task='train')
 loader = build_dataloader('CrowdHuman/annotation_train_coco_style_100.json','CrowdHuman/Images_train',config_data=cfg.data,
                           batch_size=1, rank=-1,workers=0, task='train')
-dataset_inspection(dataset, 10)
-dataset_inspection(dataset, 10)
-flag = 1
-while (flag):
-    for samples in loader:
-        if flag == 0: break
-        for idx, anns in enumerate(samples['annss']):
-            print('fin:',np.greater(anns[:,4], -0.5).sum())
-            if np.greater(anns[:,4], -0.5).sum() > 0:
-                img = samples['imgs'][idx].numpy()
-                img = np.transpose(img,(1,2,0))
-                show_bbox(img, anns)
-                flag = 0
-                break
+samples = dataset.load_sample(10)
+show_bbox(samples['img'],samples['anns'])
+dataset_inspection(dataset, 10, anntype='xywh')
+dataset_inspection(dataset, 10, anntype='xywh')
+dataset_inspection(dataset, 10, anntype='xywh')
+dataset_inspection(dataset, 10, anntype='xywh')
+# flag = 1
+# while (flag):
+#     for samples in loader:
+#         if flag == 0: break
+#         for idx, anns in enumerate(samples['annss']):
+#             print('fin:',np.greater(anns[:,4], -0.5).sum())
+#             if np.greater(anns[:,4], -0.5).sum() == 0:
+#                 img = samples['imgs'][idx].numpy()
+#                 img = np.transpose(img,(1,2,0))
+#                 show_bbox(img, anns)
+#                 flag = 0
+#                 break
