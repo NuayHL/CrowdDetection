5d8691df094247c11715adb4292b07ce5e249b71
NauyHL <liuhy.df.xin@gmail.com>
2022-09-05 21:14:12 +0900

    yolov3 step

--- trains.sh
+++ trains.sh
@@ -1,1 +1,1 @@
-python -u train.py --conf-file cfgs/YOLOv3_step_fine_tune --device 1 --batch-size 2 --workers 4
+python -u train.py --conf-file cfgs/YOLOv3_step --device 0 --batch-size 2 --workers 4 --eval-interval 5
