9acaf6940ec46d7d2c7247a92a782698096644b9
NauyHL <liuhy.df.xin@gmail.com>
2022-09-05 10:20:03 +0900

    adding some config and sh

--- trains.sh
+++ trains.sh
@@ -1,2 +1,1 @@
-python train.py --conf-file cfgs/YOLOv3_step.yaml --device 0 --batch-size 2 --workers 2
-python train.py --conf-file cfgs/YOLOv3_step.yaml --device 0 --batch-size 2 --workers 2
+python train.py --conf-file cfgs/YOLOv3_step_fine_tune --fine-tune running_log/YOLOv3_step/best_epoch.pth --device 0 --batch-size 2 --workers 4
