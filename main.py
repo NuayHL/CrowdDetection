edcfb6d48a7746f1cd88ae2f8e18ed28cdf883df
NauyHL <liuhaoyuan@akane.waseda.jp>
2022-09-06 17:34:06 +0900

    checking lr fun and adding merge from files to CN

--- main.py
+++ main.py
@@ -6,18 +6,34 @@ import torch
 import torch.nn as nn
 from torch import tensor as t
 import numpy as np
-from odcore.utils.visualization import show_bbox, LossLog
+from odcore.utils.visualization import show_bbox, LossLog, draw_scheduler
 from odcore.data.dataset import check_anno_bbox
 from odcore.data.dataset import CocoDataset
+from modelzoo.build_models import BuildModel
 from config import get_default_cfg
+from odcore.config import merge_from_files
+from odcore.utils.lr_schedular import LFScheduler
 
-# cfg = get_default_cfg()
-# cfg.merge_from_file('YOLOv3.yaml')
-#
+cfg = get_default_cfg()
+cfg.merge_from_files('cfgs/yolox')
+#merge_from_files(cfg, 'cfgs/yolox')
+
 # dataset = CocoDataset('CrowdHuman/annotation_val_coco_style_checked.json', 'CrowdHuman/Images_val', cfg.data, 'val')
 #
 # sample = dataset[110]
 #
 # show_bbox(sample['img'], sample['anns'])
 
-check_anno_bbox('CrowdHuman/annotation_val_coco_style.json')
+# check_anno_bbox('CrowdHuman/annotation_val_coco_style.json')
+# builder = BuildModel(cfg)
+# model = builder.build()
+
+
+builder = LFScheduler(cfg)
+
+
+lf = builder.get_lr_fun()
+
+draw_scheduler(lf, cfg.training.final_epoch)
+
+
