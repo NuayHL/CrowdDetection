2670c0114a3292b7ae9a5ec2477aba3cd0454ad7
NauyHL <liuhy.df.xin@gmail.com>
2022-09-16 21:07:41 +0900

    start yolox ota training

--- trains.sh
+++ trains.sh
@@ -1,5 +1,5 @@
-python -u train.py --conf-file cfgs/yolox_coco\
-                   --device 0 --batch-size 32 --accumu 1 --workers 8 --eval-interval 10
+python -u train.py --conf-file cfgs/yolox_ota\
+                   --device 0 --batch-size 32 --accumu 1 --workers 8 --eval-interval 20
 echo " "
 echo "Press AnyKey to Continue"
 read -n 1
