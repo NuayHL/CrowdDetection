0633bdc3d5c45a0c12a3ee912384486875667552
NauyHL <liuhy.df.xin@gmail.com>
2022-09-20 11:42:42 +0900

    modifying cosine lr

--- test/lr_scheduler.py
+++ test/lr_scheduler.py
@@ -1,3 +1,7 @@
+import os
+import sys
+path = os.getcwd()
+sys.path.append(os.path.join(path, 'odcore'))
 from odcore.utils.visualization import draw_scheduler
 from config import get_default_cfg
 from odcore.utils.lr_schedular import LFScheduler
@@ -7,4 +11,4 @@ cfg.merge_from_files('cfgs/yolox')
 
 builder = LFScheduler(cfg)
 lf = builder.get_lr_fun()
-draw_scheduler(lf, cfg.training.final_epoch)
+draw_scheduler(lf, 400)
