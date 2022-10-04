fb02f39a97978dd2dbfbb14ee856a852f5a4feb7
NuayHL <liuhy.df.xin@gmail.com>
2022-10-04 11:01:37 +0900

    add poly lr_scheduler

--- test/lr_scheduler.py
+++ test/lr_scheduler.py
@@ -1,13 +1,17 @@
 import os
 import sys
 path = os.getcwd()
-sys.path.append(os.path.join(path, 'odcore'))
+sys.path.append(os.path.join(path, '../odcore'))
+os.chdir(os.path.join(path, '..'))
 from odcore.utils.visualization import draw_scheduler
 from config import get_default_cfg
 from odcore.utils.lr_schedular import LFScheduler
 
+import matplotlib
+matplotlib.use('TkAgg')
+
 cfg = get_default_cfg()
-cfg.merge_from_files('cfgs/yolox')
+cfg.merge_from_files('cfgs/yolox_pd_csp_poly')
 
 builder = LFScheduler(cfg)
 lf = builder.get_lr_fun()
