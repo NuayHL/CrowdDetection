6a56dbae9036af77b1e47606e66d897179a908f8
NuayHL <liuhy.df.xin@gmail.com>
2022-10-12 11:31:44 +0900

    new cfg

--- trains.sh
+++ trains.sh
@@ -1,5 +1,5 @@
 python -u train.py --conf-file cfgs/yolox_free_s2_ignored\
-                   --device 0 --batch-size 1 --accumu 1 --workers 1 --eval-interval 10 --save-interval 50
+                   --device 0 --batch-size 32 --accumu 1 --workers 16 --eval-interval 10 --save-interval 50
 
 echo " "
 echo "Press AnyKey to Continue"
