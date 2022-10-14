5252f502f184e565a850ced27099dff1ff96e528
NuayHL <liuhy.df.xin@gmail.com>
2022-10-14 16:50:03 +0900

    t ab

--- modelzoo/neck/build.py
+++ modelzoo/neck/build.py
@@ -2,7 +2,7 @@ import torch
 from modelzoo.neck.pafpn import PAFPN
 from modelzoo.neck.retinafpn import Retina_neck
 from modelzoo.neck.yolov3neck import Yolov3_neck
-#from modelzoo.neck.abneck import ABNeck
+from modelzoo.neck.abneck import ABNeck
 def build_neck(name):
     '''return neckClass, ratio on p3c'''
     if name == 'yolov3_neck':
@@ -11,8 +11,8 @@ def build_neck(name):
         return Retina_neck, 1.0
     elif name == 'pafpn':
         return PAFPN, 1.0
-    # elif name == 'abneck':
-    #     return ABNeck, 1.0
+    elif name == 'abneck':
+        return ABNeck, 1.0
 
     else:
         raise NotImplementedError('No neck named %s'%name)
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     p4 = torch.rand((1, 512, 16, 16))
     p5 = torch.rand((1, 1024, 8, 8))
 
-    fpn = ABNeck(p3c=256, width=1.0)
+    fpn = PAFPN(256)
     print("Num of Parameters: %.2fM" % (numofParameters(fpn) / 1e6))
-    result = fpn(p3,p4,p5)
+    result = fpn(p3, p4, p5)
     for i in result:
         print(i.shape)
