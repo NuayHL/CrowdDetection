781dd8cba7c8cc9bab075fd26ff6081820e474bf
NuayHL <liuhy.df.xin@gmail.com>
2022-11-19 04:27:51 +0900

    draw mr val

--- drawval.py
+++ drawval.py
@@ -10,16 +10,19 @@ def get_args_parser(add_help=True):
     parser = argparse.ArgumentParser(description='Args for val drawing', add_help=add_help)
     parser.add_argument('--val-log', default='', type=str, help='full path of _loss.log file')
     parser.add_argument('-exp', default='', type=str, help='experiment name')
+    parser.add_argument('-type', default='coco', type=str, help='Val metric type: coco or mr')
     return parser
 
 
 def main(args):
     if args.val_log != '':
         vallog = ValLog(args.val_log)
-        vallog.coco_draw(zero_start=True)
     else:
         vallog = ValLog('running_log/%s/%s_val.log' % (args.exp, args.exp))
+    if args.type == 'coco':
         vallog.coco_draw(zero_start=True)
+    else:
+        vallog.mr_draw(zero_start=True)
 
 if __name__ == '__main__':
     args = get_args_parser().parse_args()
