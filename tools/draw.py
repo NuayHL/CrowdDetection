7bbab93d12097bd0007e1d14acb43bf375202449
NuayHL <liuhy.df.xin@gmail.com>
2022-11-22 17:30:12 +0900

    draw compare

---
+++ tools/draw.py
@@ -0,0 +1,44 @@
+import os
+path = os.getcwd()
+os.chdir(os.path.join(path, '..'))
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+from odcore.utils.visualization import ValLog
+
+def val_compare(*val_files, metric_type='mr', short_exp_name=True):
+    metric_name = 'IoU.5' if metric_type == 'coco' else 'AP'
+    val_logs = list()
+    for val_file in val_files:
+        if short_exp_name:
+            val_file = 'running_log/'+val_file+'/'+val_file+'_val.log'
+        temp_log_obj = ValLog(val_file)
+        if metric_type == 'mr':
+            temp_log_obj.mr_val(zero_start=False)
+        elif metric_type == 'coco':
+            temp_log_obj.coco_val(zero_start=False)
+        else:
+            raise NotImplementedError
+        val_logs.append(temp_log_obj)
+
+    fig, ax = plt.subplots()
+
+    for val_log in val_logs:
+        ax.plot(*val_log.data[metric_name])
+
+    fig.legend(val_files)
+
+    ax.set(xlabel="Epochs", ylabel=metric_name, title="Compare...")
+    ax.grid()
+    fig.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    val_compare('YOLOX_ori',
+                'YOLOX_R',
+                metric_type='coco')
+
+
+
