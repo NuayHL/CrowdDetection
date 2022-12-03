9ac3d2b88385564b076f6f397005aca7d95091d0
NuayHL <liuhy.df.xin@gmail.com>
2022-12-03 16:47:32 +0900

    mov eval

--- eval.py
+++ eval.py
@@ -13,18 +13,8 @@ def main(args):
     builder = BuildModel(cfg)
     model = builder.build()
     model.set(args, 0)
-    flag = 0
-    if args.type == 'mip':
-        print('using mip for eval, firstly eval mr')
-        args.type = 'mr'
-        flag = 1
     evaluate = Eval(cfg, args, model, 0)
     evaluate.eval()
-    if flag == 1:
-        json_pre_file = os.path.splitext(args.ckpt_file)[0] + '_evalresult.json'
-        odgt_file = coco2odgt_det(json_pre_file)
-        os.system('python evaluate/compute_APMR.py --detfile %s --target_key box' % odgt_file)
-        os.system('python evaluate/compute_JI.py --detfile %s --target_key box' % odgt_file)
 
 if __name__ == "__main__":
     print(os.getcwd())
